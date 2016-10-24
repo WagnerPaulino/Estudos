cd59e8c64a1c4adae524443ded576ee96ce2857a
wagner <wagner@wagner-Infoway>
2016-10-23 22:20:59 -0300

    Aula de pilha parte 1

---
+++ estudoC++/src/arquivoH/Pilha.h
@@ -0,0 +1,21 @@
+/*
+ * Pilha.h
+ *
+ *  Created on: 23 de out de 2016
+ *      Author: wagner
+ */
+
+#ifndef ARQUIVOH_PILHA_H_
+#define ARQUIVOH_PILHA_H_
+
+namespace std {
+
+class Pilha {
+public:
+	Pilha();
+	virtual ~Pilha();
+};
+
+} /* namespace std */
+
+#endif /* ARQUIVOH_PILHA_H_ */
