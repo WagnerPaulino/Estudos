15f919de722e104fb3f5d6961383e2ec57885f62
wagner <wagner@wagner-Infoway>
2016-10-23 22:24:16 -0300

    commit aula de pilha

--- estudoC++/src/arquivoCPP/Pilha.cpp
+++ estudoC++/src/arquivoCPP/Pilha.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <stack>//precisa dessa biblioteca para se trabalhar com pilhas
+//#include <iostream>
+//#include <stack>//precisa dessa biblioteca para se trabalhar com pilhas
 
-using namespace std;
+//using namespace std;
 
-int main(){
+//int main(){
 
-	stack <string> cartas;//criando uma pilha
+	//stack <string> cartas;//criando uma pilha
 
-	cartas.push("Rei de Copas");//metodo push adiciona elementos em uma pilha
-	cartas.push("Rei de Espadas");
-	cartas.push("Rei de Ouros");
-	cartas.push("Rei de Paus");
+	//cartas.push("Rei de Copas");//metodo push adiciona elementos em uma pilha
+	//cartas.push("Rei de Espadas");
+	//cartas.push("Rei de Ouros");
+	//cartas.push("Rei de Paus");
 
-	cout << "Tamanho da pilha: "<<cartas.size()<<"\n";
+	//cout << "Tamanho da pilha: "<<cartas.size()<<"\n";
 
-	cout << "Carta do topo: "<<cartas.top();//retorna o elemento do topo
+	//cout << "Carta do topo: "<<cartas.top();//retorna o elemento do topo
 
 	//cartas.pop();//retira o elemento do topo da pilha
 	//cout << "Tamanho da pilha apos o metodo pop: "<<cartas.size()<<"\n";
 
-	return 0;
-}
+	//return 0;
+//}
