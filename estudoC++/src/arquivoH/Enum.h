22d76a9ae061cc932d5aa9a71fdac5a6acac561f
wagner <wagner@wagner-Infoway>
2016-10-23 21:52:56 -0300

    Aula sobre o enum

---
+++ estudoC++/src/arquivoH/Enum.h
@@ -0,0 +1,21 @@
+/*
+ * Enum.h
+ *
+ *  Created on: 23 de out de 2016
+ *      Author: wagner
+ */
+
+#ifndef ARQUIVOH_ENUM_H_
+#define ARQUIVOH_ENUM_H_
+
+namespace std {
+
+class Enum {
+public:
+	Enum();
+	virtual ~Enum();
+};
+
+} /* namespace std */
+
+#endif /* ARQUIVOH_ENUM_H_ */
