00df3ed8b932517374b5e9814eacf2303557e643
wagner <wagner@wagner-Infoway>
2016-10-24 21:25:32 -0300

    Correção de comentarios

--- estudoC++/src/arquivoCPP/Enum.cpp
+++ estudoC++/src/arquivoCPP/Enum.cpp
@@ -1,19 +1,19 @@
-//#include <iostream>
+/*#include <iostream>
 
-//using namespace std;
+using namespace std;
 
-//int main() {
+int main() {
 
-	//enum armas{fuzil=100, resvolver=8, rifle=12, escopeta=6};//Declara um enum
-	/*Lembrando que se só fosse atribuido o valor 100 ao fuzil os outros iriam ter
-	 * automaticamente os valores 101, 102, e 103 e assim por diante*/
-	//armas armaSel;
-	//int num;
+	enum armas{fuzil=100, resvolver=8, rifle=12, escopeta=6};//Declara um enum
+	//Lembrando que se só fosse atribuido o valor 100 ao fuzil os outros iriam ter
+	//automaticamente os valores 101, 102, e 103 e assim por diante
+	armas armaSel;
+	int num;
 
-	//armaSel=fuzil;
+	armaSel=fuzil;
 
-	//cout << armaSel;
+	cout << armaSel;
 
-	//return 0;
-//}
+	return 0;
+}*/
 
