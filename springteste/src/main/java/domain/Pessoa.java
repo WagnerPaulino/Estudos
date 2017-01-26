5aad753ba305c47e9f2ee1ebcc9fd314b9e67892
wagner <wagner@rpawsds09.redetre.jus.br>
2017-01-26 09:15:50 -0300

    commit funcionando sem a Entity

---
+++ springteste/src/main/java/domain/Pessoa.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Pessoa implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+}
