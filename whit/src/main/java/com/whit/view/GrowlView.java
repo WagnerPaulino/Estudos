524c4be7c3de44215715b308a5e7e5fb1f0d5bb2
wagner <wagner@wagner-Infoway>
2017-02-01 19:43:31 -0300

    Ajustes

--- whit/src/main/java/com/whit/view/GrowlView.java
+++ whit/src/main/java/com/whit/view/GrowlView.java
@@ -8,16 +8,20 @@ import javax.faces.context.FacesContext;
 public class GrowlView {
 
 	private String message;
+	
 
 	public String getMessage() {
+	
 		return message;
 	}
 
 	public void setMessage(String message) {
+	
 		this.message = message;
 	}
 
 	public void saveMessage() {
+	
 		FacesContext context = FacesContext.getCurrentInstance();
 
 		context.addMessage(null, new FacesMessage("Successful", "Your message: " + message));
