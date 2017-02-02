eb0f0a75860d12b55c4cc1fc511a1dadbb73d76f
wagner <wagner@wagner-Infoway>
2017-02-02 15:25:03 -0300

    *ajustes

--- whit/src/main/java/com/whit/view/GrowlView.java
+++ whit/src/main/java/com/whit/view/GrowlView.java
@@ -1,27 +1,33 @@
 package com.whit.view;
 
+import java.io.Serializable;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
-public class GrowlView {
+@ViewScoped
+public class GrowlView implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String message;
-	
 
 	public String getMessage() {
-	
+
 		return message;
 	}
 
 	public void setMessage(String message) {
-	
+
 		this.message = message;
 	}
 
 	public void saveMessage() {
-	
 		FacesContext context = FacesContext.getCurrentInstance();
 
 		context.addMessage(null, new FacesMessage("Successful", "Your message: " + message));
