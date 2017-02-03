99804c2d2ebfe6348afc563cfd47d41b479b67f8
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-03 10:53:11 -0300

    hello

--- projectf/src/main/java/com/projectf/ProjectfApplication.java
+++ projectf/src/main/java/com/projectf/ProjectfApplication.java
@@ -2,11 +2,21 @@ package com.projectf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class ProjectfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectfApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String teste(){
+		return "teste";
 	}
 }
