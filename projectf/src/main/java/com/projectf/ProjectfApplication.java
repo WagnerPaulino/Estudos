1bb84ddb10b612cb0142b76ea9583831bcd87e19
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-03 09:23:53 -0300

    primeiro commit

---
+++ projectf/src/main/java/com/projectf/ProjectfApplication.java
@@ -0,0 +1,12 @@
+package com.projectf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfApplication.class, args);
+	}
+}
---
+++ projectf/src/main/resources/db/migration/V1__Create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pessoa(
+	id INT AUTO_INCREMENT,
+	name VARCHAR(32) NOT NULL,
+	PRIMARY KEY(id)
+);
