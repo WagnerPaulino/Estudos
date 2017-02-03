6cc8bf9c0d86c78cbdb0a211bf8850c9e1c51d96
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-03 08:16:35 -0300

    *ajustes

--- whit/src/main/java/com/whit/view/GrowlView.java
+++ whit/src/main/java/com/whit/view/GrowlView.java
@@ -4,11 +4,9 @@ import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
-@ViewScoped
 public class GrowlView implements Serializable {
 
 	/**
