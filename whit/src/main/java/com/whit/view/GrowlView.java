fc02a177569199f61ae9815ebf2de9a658f5ec95
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-03 11:06:17 -0300

    teste em casa

--- whit/src/main/java/com/whit/view/GrowlView.java
+++ whit/src/main/java/com/whit/view/GrowlView.java
@@ -2,9 +2,7 @@ package com.whit.view;
 
 import java.io.Serializable;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class GrowlView implements Serializable {
@@ -13,22 +11,8 @@ public class GrowlView implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
 
-	public String getMessage() {
-
-		return message;
-	}
-
-	public void setMessage(String message) {
-
-		this.message = message;
-	}
-
-	public void saveMessage() {
-		FacesContext context = FacesContext.getCurrentInstance();
-
-		context.addMessage(null, new FacesMessage("Successful", "Your message: " + message));
-		context.addMessage(null, new FacesMessage("Second Message", "Additional Message Detail"));
+	public String getTeste(){
+		return "teste";
 	}
 }
