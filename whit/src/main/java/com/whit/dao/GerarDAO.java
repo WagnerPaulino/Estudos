34e2a83f50b7bedb442e4389b29b8079fca04f76
wagner <wagner@wagner-Infoway>
2017-02-06 18:52:17 -0300

    -Retirada do jpa... Tentativa fail

--- whit/src/main/java/com/whit/dao/GerarDAO.java
+++ whit/src/main/java/com/whit/dao/GerarDAO.java
@@ -1,13 +1,4 @@
 package com.whit.dao;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class GerarDAO {
-
-	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("message");
-		factory.close();
-
-	}
 }
