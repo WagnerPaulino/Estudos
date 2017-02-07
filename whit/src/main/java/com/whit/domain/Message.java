49a4dad420baf7ca1212629ceef418980dd6b105
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-07 09:28:54 -0300

    +jpa--------testar

--- whit/src/main/java/com/whit/domain/Message.java
+++ whit/src/main/java/com/whit/domain/Message.java
@@ -2,15 +2,26 @@ package com.whit.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "message")
 public class Message implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@Column
 	private String message;
 
 	public Long getId() {
