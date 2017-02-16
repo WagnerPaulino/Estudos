549b6cee6ab3f2e73a631d68fd6501bfaa82fab0
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-16 09:54:13 -0300

    commit do guia do angular 2-------incompleto

--- lab-angular/src/app/app.component.ts
+++ lab-angular/src/app/app.component.ts
@@ -1,26 +1,11 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero.name}}</h2>
-  <p>Heroes:</p>
-  <ul>
-    <li *ngFor="let hero of heroes">
-      {{ hero.name }}
-      </li>
-  </ul>
-  <p *ngIf="heroes.length > 3">There are many heroes!</p>
-`
+    <hero-list></hero-list>
+    <hero-list-promise></hero-list-promise>
+    <my-wiki></my-wiki>
+    <my-wiki-smart></my-wiki-smart>
+  `
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'),
-    new Hero(13, 'Bombasto'),
-    new Hero(15, 'Magneta'),
-    new Hero(20, 'Tornado')
-  ];
-  myHero = this.heroes[0];
-}
+export class AppComponent { }
--- lab-angular/src/app/app.module.ts
+++ lab-angular/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule
   ],
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
