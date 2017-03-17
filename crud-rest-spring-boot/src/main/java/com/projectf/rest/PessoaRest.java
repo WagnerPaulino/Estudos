0f6e38501c6876a122201892ceeeef0f3ac73546
wagner <wagner@rpawsds09.redetre.jus.br>
2017-03-17 09:55:22 -0300

    coerencia de nomes

--- crud-rest-spring-boot/src/main/java/com/projectf/rest/PessoaRest.java
+++ crud-rest-spring-boot/src/main/java/com/projectf/rest/PessoaRest.java
@@ -1,11 +1,8 @@
 package com.projectf.rest;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
